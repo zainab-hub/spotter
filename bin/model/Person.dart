72d23597076e419d13de29558480bd90d61eb3a2
Zainab <zainab.talib85@gmail.com>
2025-02-02 16:35:36 +0100

    First commit

---
+++ bin/model/Person.dart
@@ -0,0 +1,11 @@
+// ignore: file_names
+class Person {
+  String namn;
+  int personnummer;
+  Person(this.namn,this.personnummer);
+}
+
+class PersonRepository {
+  
+
+}
---
+++ bin/spotter.dart
@@ -0,0 +1,8 @@
+import 'package:args/args.dart';
+
+const String version = '0.0.1';
+
+void main(List<String> arguments) {
+  
+  
+}
