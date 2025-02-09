845b56d908abbd87cced96cd50619de03a9e7ac6
Zainab-hub <zainab.talib85@gmail.com>
2025-02-09 22:49:01 +0100

    stdin

--- bin/Repository/PersonRepository.dart
+++ bin/Repository/PersonRepository.dart
@@ -15,10 +15,22 @@ Person getById(id) {
 
   return persons[id];
 }
-void update(item) {
+void update(Person oldperson , Person newperson) {
+  int index = persons.indexOf(oldperson);
+  if (index !=-1)
+  {
+    persons[index] = newperson; // Replace old person with new person
+  }
+  else {
+    print("Person is not found");
+  }
+
+  }
+
+ // Method to remove an ID by value
+void delete( Person id) {
+   persons.remove(id);
+}
 
 }
-void delete(item) {
-  
-}
-}
+
--- bin/spotter.dart
+++ bin/spotter.dart
@@ -1,8 +1,19 @@
 import 'package:args/args.dart';
+import 'model/Person.dart';
+import 'dart:io';
 
 const String version = '0.0.1';
 
 void main(List<String> arguments) {
+  stdout.writeln('Welcome to Spotter\nWhat do you want to handle? \n 1.Person \n 2.Vehicle \n 3.ParkingSpace \n 4.Parking \n 5.Close\nChoose alternative between (1-5) ');
+  var input = stdin.readLineSync();
+  stdout.writeln('You typed: $input');
+
+  var name = stdin.readLineSync();
+
+
+
   
-  
+
+
 }
